13ce4dc964b69b19f52cbd58f51891125a1b73bc
beee52 <dien.cai@gmail.com>
2026-03-06 10:19:14 +1100

    Working React frontend scaffold

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Airline Pay Rules Platform</h1>
+      <p>Enterprise agreement interpretation engine</p>
+
+      <div style={{marginTop: "40px"}}>
+        <button>Airlines</button>
+        <button>Enterprise Agreements</button>
+        <button>Rules Engine</button>
+        <button>Roster Upload</button>
+        <button>Payroll Output</button>
+      </div>
+    </>
+  )
+}
+
+export default App
